470b7b9b3ff35ff0d149fea022070c14306e900a
mario Tabalí <tabali.mario@gmail.com>
2025-07-10 15:49:58 +0000

    test

--- src/dango_stack/middleware/auth.clj
+++ src/dango_stack/middleware/auth.clj
@@ -1,6 +1,7 @@
 (ns dango-stack.middleware.auth
   (:require [dango-stack.util.jwt :refer [decode-jwt]]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [clojure.string :as str]))
 
 (def public-paths #{"/api/login" "/api/register"})
 
@@ -9,7 +10,8 @@
     (let [uri (:uri req)]
       (if (contains? public-paths uri)
         (handler req)
-        (let [jwt-token (get-in req [:headers "authorization"])]
+        (let [auth-header (get-in req [:headers "authorization"])
+          jwt-token   (some-> auth-header (str/replace #"(?i)^Bearer " ""))]
           (println (decode-jwt jwt-token))
           (if-let [user-info (decode-jwt jwt-token)]
             (do
