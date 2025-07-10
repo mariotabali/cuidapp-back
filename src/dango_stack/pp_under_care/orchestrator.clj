023c2a142b4df01ca69c3463decfac4a7cc15eaf
mario Tabalí <tabali.mario@gmail.com>
2025-07-10 16:03:40 +0000

    test

--- src/dango_stack/pp_under_care/orchestrator.clj
+++ src/dango_stack/pp_under_care/orchestrator.clj
@@ -1,9 +1,7 @@
 
 (ns dango-stack.pp-under-care.orchestrator
-  (:require [dango-stack.util.jwt :refer [decode-jwt]]
-            [dango-stack.pp-under-care.db :refer [get-pp-under-care]]))
+  (:require 
+   [dango-stack.pp-under-care.db :refer [get-pp-under-care]]))
 
-(defn pp-under-care-orchestrator [req]
-  (let [jwt-token (get-in req [:headers "authorization"])
-        username (:email (decode-jwt jwt-token))]
-    (get-pp-under-care username)))
+(defn pp-under-care-orchestrator [username] 
+    (get-pp-under-care username))
