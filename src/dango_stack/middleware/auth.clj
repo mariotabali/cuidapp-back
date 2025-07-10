f0fb0b3cbf0e33323dae74b2bf6153ec6dd6f2f6
mario Tabalí <tabali.mario@gmail.com>
2025-07-10 13:56:05 +0000

    fixed middleware to exclude login

--- src/dango_stack/middleware/auth.clj
+++ src/dango_stack/middleware/auth.clj
@@ -1,13 +1,15 @@
-(ns dango-stack.middleware.auth
-  (:require [dango-stack.util.jwt :refer [decode-jwt]]
-            [clojure.tools.logging :as log]))
+(def public-paths #{"﻿/api/login" "/api/register"})
 
 (defn wrap-authentication [handler]
   (fn [req]
-    (let [jwt-token (get-in req [:headers "authorization"])]
-      (if-let [user-info (decode-jwt jwt-token)] 
-        (do
-          (log/info "Authenticated request by user:" (:email user-info))
-          (handler (assoc req :user user-info)))
-        ;; Respond with 401 if token is invalid or missing
-        {:status 401 :headers {"Content-Type" "application/json"} :body {:error "Unauthorized"}}))))
+    (let [uri (:uri req)]
+      (if (contains? public-paths uri)
+        (handler req)
+        (let [jwt-token (get-in req [:headers "authorization"])]
+          (if-let [user-info (decode-jwt jwt-token)]
+            (do
+              (log/info "Authenticated request by user:" (:email user-info))
+              (handler (assoc req :user user-info)))
+            {:status 401
+             :headers {"Content-Type" "application/json"}
+             :body {:error "Unauthorized"}}))))))
