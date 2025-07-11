19a7be252c0f3b8c49a8a9cc1a945659a04f6b7d
mario Tabalí <tabali.mario@gmail.com>
2025-07-11 16:50:34 +0000

    fixed ssl

--- src/dango_stack/register/email.clj
+++ src/dango_stack/register/email.clj
@@ -8,7 +8,7 @@
                      :user     (env :smtp-user)
                      :pass     (env :smtp-pass)
                      :port     (Integer/parseInt (or (env :smtp-port) "587"))
-                     :tls      true}
+                     :ssl      true}
         from (or (env :smtp-from) (env :smtp-user))
         subject "Activate your account"
         body (str "Welcome!\n\nPlease activate your account by visiting the following link:\n\n"
